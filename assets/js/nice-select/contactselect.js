0a3b422fe7826214b388ba0f935f9bb9838a78d8
it27427 <istiak.tushar.qtec@gmail.com>
2023-04-13 15:41:45 +0600

    contact-section completed

---
+++ assets/js/nice-select/contactselect.js
@@ -0,0 +1,9 @@
+NiceSelect.bind(document.getElementById("numberofguests"), {
+  searchable: false,
+  placeholder: 'Number of guests'
+});
+
+NiceSelect.bind(document.getElementById("mealpreferences"), {
+  searchable: false,
+  placeholder: 'Meal preferences'
+});
