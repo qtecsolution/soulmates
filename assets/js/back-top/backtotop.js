0eb11ed816f0fe81bda05988516188600f221555
it27427 <istiak.tushar.qtec@gmail.com>
2023-04-26 12:56:10 +0600

    back-to-top added

---
+++ assets/js/back-top/backtotop.js
@@ -0,0 +1,18 @@
+up({
+  // bottom position
+  bottom: '2.75rem',
+  // right position
+  right: '1.5rem',
+  // width
+  width: '2.75rem',
+  // height
+  height: '2.75rem',
+  // background color
+  bg: 'hsl(231, 71%, 15%)',
+  // custom icon
+  src: '../../assets/cursor/chevron-up-solid.svg',
+  // distance from the top to show the back to top button
+  whenShow: 400,
+  // circular button?
+  circle: true
+});
