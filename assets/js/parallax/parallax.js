07aec2547edba636639f3a533f51aeee907a6ef4
it27427 <istiak.tushar.qtec@gmail.com>
2023-05-08 17:08:21 +0600

    parallax home-1 completed

--- assets/js/parallax/parallax.js
+++ assets/js/parallax/parallax.js
@@ -1,7 +1,17 @@
-const rellax = new Rellax('.rellax', {
-  speed: -2,
-  center: true,
-  round: false,
-  vertical: true,
-  horizontal: false
-});
+function parallax() {
+  let parallaxes = document.querySelectorAll(".parallax");
+
+  for (let i = 0; i < parallaxes.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = parallaxes[i].getBoundingClientRect().top;
+    let elementVisible = 10;
+
+    if (elementTop < (windowHeight - elementVisible)) {
+      parallaxes[i].classList.add("active");
+    } else {
+      parallaxes[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", parallax);
