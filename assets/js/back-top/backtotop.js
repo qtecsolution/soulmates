8b16f90e061a8a8ec59cb8229bb9085ce2af5def
it27427 <istiak.tushar.qtec@gmail.com>
2023-05-09 17:35:26 +0600

    message gap fixed

--- assets/js/back-top/backtotop.js
+++ assets/js/back-top/backtotop.js
@@ -10,7 +10,7 @@ up({
   // background color
   bg: 'hsl(231, 71%, 15%)',
   // custom icon
-  src: '../../assets/cursor/chevron-up-solid.svg',
+  // src: '../../assets/cursor/chevron-up-solid.svg',
   // distance from the top to show the back to top button
   whenShow: 400,
   // circular button?
