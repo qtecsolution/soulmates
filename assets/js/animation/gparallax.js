1d871fed4b1143371cfd6c449251e833eb4ac3c2
it27427 <istiak.tushar.qtec@gmail.com>
2023-05-25 16:35:34 +0600

    template-updated

--- assets/js/animation/gparallax.js
+++ assets/js/animation/gparallax.js
@@ -1,21 +1,21 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".animitem").forEach(function(container) {
-  let anim = container.querySelector(".anim");
+// gsap.utils.toArray(".animitem").forEach(function(container) {
+//   let anim = container.querySelector(".anim");
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: container,
-      scrub: true,
-      pin: false,
-    },
-  });
+//   let tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: container,
+//       scrub: true,
+//       pin: false,
+//     },
+//   });
 
-  tl.from(anim, {
-    yPercent: 0,
-    ease: "none",
-  }).to(anim, {
-    yPercent: 10,
-    ease: "none",
-  }); 
-});
+//   tl.from(anim, {
+//     yPercent: 0,
+//     ease: "none",
+//   }).to(anim, {
+//     yPercent: 10,
+//     ease: "none",
+//   }); 
+// });
